7c736ad8a76ec49d07c0048eff44a8b274263476
imdoarda <maria.galvao@alunoescolassociais.org.br>
2023-04-28 08:27:47 -0300

    mensagem

---
+++ parteII.js
@@ -0,0 +1,12 @@
+function Verifica(){
+    let cep = document.getElementById('enderecoCEP').value;
+    let rua = document.getElementById('nomeRua').value;
+    let number = document.getElementById('enderecoNumber').value;
+    let reference = document.getElementById('enderecoReference').value;
+
+    if(!cep || !rua || !number || !reference){
+        alert("Campos de preenchimento obrigatório. Favor preencher.");
+     }else{
+      window.open("confirma.html", "_self")
+  };
+};
