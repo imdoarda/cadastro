4afda6ff04d84aeec1dfcbbd8b68c67815cdeb0e
Imdoarda <dudinha.mgalvao@gmail.com>
2023-08-16 09:03:01 -0300

    adicionei campos de endereço

--- parteII.js
+++ parteII.js
@@ -3,8 +3,10 @@ function Verifica(){
     let rua = document.getElementById('nomeRua').value;
     let number = document.getElementById('enderecoNumber').value;
     let reference = document.getElementById('enderecoReference').value;
+    let city = document.getElementById('enderecoCidade').value;
+    let state = document.getElementById('enderecoEstado').value;
 
-    if(!cep || !rua || !number || !reference){
+    if(!cep || !rua || !number || !reference || !city || !state){
         alert("Campos de preenchimento obrigatório. Favor preencher.");
      }else{
       window.open("confirma.html", "_self")
